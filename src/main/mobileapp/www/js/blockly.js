f9891a5719948f33e3d258489a61886ece873183
Sara Queiroz <sara.siqueira@cronapp.io>
2022-04-20 12:30:19 +0000

    First commit...

---
+++ src/main/mobileapp/www/js/blockly.js
@@ -0,0 +1,12 @@
+window.fixedTimeZone = true;
+window.timeZone = 'UTC';
+window.timeZoneOffset = 0;
+window.blockly = window.blockly || {};
+window.blockly.js = window.blockly.js || {};
+window.blockly.js.blockly = window.blockly.js.blockly || {};
+window.translations = window.translations || {};
+window.translations.localesKeys = ['pt_br','en_us'];
+window.translations.localesRef =  {"pt*":"pt_br","*":"pt_br","en*":"en_us"};
+document.write("<script src='js/blockly/auth/ChangePassword.blockly.js?e3858ce02693c16ac9ca8388ff344acc'></script>");
+document.write("<script src='js/blockly/auth/Home.blockly.js?2da02c5ead31c34ab67ee861caf77bc9'></script>");
+document.write("<script src='js/blockly/auth/Login.blockly.js?cc551bda44726a6090736c4b9f7a9ee1'></script>");
